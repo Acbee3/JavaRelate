8126639f0cb59d258d0fe81bb47ad853e866670e
Acbee3 <2463254281@qq.com>
2024-10-02 17:59:25 +0800

    mybatis mapper 配置

---
+++ com.lc.test01/src/main/java/com/lc/mapper/CountryMapper.java
@@ -0,0 +1,9 @@
+package com.lc.mapper;
+
+import java.util.List;
+
+public interface CountryMapper {
+    List selectAllCountries();
+
+
+}
---
+++ com.lc.test01/src/main/java/com/lc/test01/Test.java
@@ -0,0 +1,32 @@
+package com.lc.test01;
+
+import com.lc.mapper.CountryMapper;
+import com.lc.work.Country;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+//        List ctryList = sqlSession.selectList("selectAllCountries");
+//        for(int i = 0; i < ctryList.size(); i++) {
+//            Country ctry = (Country) ctryList.get(i);
+//            System.out.println("country: " + ctry.getRegion_id() + "-" + ctry.getCountry_name() + "-" + ctry.getCountry_id());
+//        }
+        CountryMapper ctryMapper = sqlSession.getMapper(CountryMapper.class);
+        List ctryList = ctryMapper.selectAllCountries();
+        for(int i = 0; i < ctryList.size(); i++) {
+            Country ctry = (Country) ctryList.get(i);
+            System.out.println("country: " + ctry.getRegion_id() + "-" + ctry.getCountry_name() + "-" + ctry.getCountry_id());
+        }
+    }
+}
