fed04522054fb414c188bf29d3d205429c0058c1
Acbee3 <liuchaoacbee3@163.com>
2024-10-13 23:51:28 +0800

    代码修改

--- com.lc.base/src/main/java/example/NarcisssiticNumber.java
+++ com.lc.base/src/main/java/example/NarcisssiticNumber.java
@@ -4,8 +4,8 @@ public class NarcisssiticNumber {
     public static void main(String[] args) {
         for (int i = 100; i < 1000; i++) {
             int hundreds = i / 100;
-            int tens = i / 10 / 10;
-            int ones = i / 10 / 10 % 10;
+            int tens = (i - hundreds * 100) / 10;
+            int ones = i % 10;
             if (hundreds * hundreds * hundreds + tens * tens * tens + ones * ones * ones == i) {
                 System.out.println("num: " + i);
             }
